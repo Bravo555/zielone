cf1c3cb1cf84a6d85d085392852a5b37d434e7a5
Marcel Guzik <mguzik555@gmail.com>
2022-02-20 18:25:04 +0100

    implemented user list update and feedback submit

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,43 @@
 use std::{
     collections::HashMap,
-    sync::{Arc, Mutex},
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+    time::Duration,
 };
 
-use futures_util::{FutureExt, SinkExt, StreamExt};
-use tokio::sync::{mpsc, RwLock};
+use futures_util::{SinkExt, StreamExt};
+use tokio::sync::RwLock;
 use warp::{
     ws::{Message, WebSocket},
     Filter,
 };
 
-type Users = Arc<RwLock<HashMap<i32, tokio::sync::mpsc::UnboundedSender<Message>>>>;
+#[derive(Debug, Copy, Clone)]
+enum UserFeedback {
+    Green,
+    Red,
+}
+
+impl ToString for UserFeedback {
+    fn to_string(&self) -> String {
+        match self {
+            UserFeedback::Green => "zielone".to_string(),
+            UserFeedback::Red => "czerwone".to_string(),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct User {
+    tx: tokio::sync::mpsc::UnboundedSender<Message>,
+    feeback: Option<UserFeedback>,
+}
+
+type Users = Arc<RwLock<HashMap<usize, User>>>;
+
+static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
 
 #[tokio::main]
 async fn main() {
@@ -35,29 +62,117 @@ async fn main() {
 }
 
 async fn user_connected(websocket: WebSocket, users: Users) {
-    let (mut tx, mut rx) = websocket.split();
-    let lock = users.write().await;
+    let (mut ws_tx, mut ws_rx) = websocket.split();
+    let (chan_tx, mut chan_rx) = tokio::sync::mpsc::unbounded_channel();
 
-    let new_id = lock.len() as i32 + 1;
+    let new_id = NEXT_USER_ID.fetch_add(1, Ordering::Relaxed);
 
-    tx.send(Message::text(format!("your id: {new_id}")))
+    let mut lock = users.write().await;
+
+    let user = User {
+        tx: chan_tx.clone(),
+        feeback: None,
+    };
+
+    lock.insert(new_id, user);
+    drop(lock);
+
+    ws_tx
+        .send(Message::text(format!("your id: {new_id}")))
         .await
         .unwrap();
 
-    while let Some(message) = rx.next().await {
+    broadcast_user_list(&users).await;
+
+    tokio::spawn(async move {
+        while let Some(message) = chan_rx.recv().await {
+            ws_tx.send(message).await.unwrap_or_else(|e| {
+                eprintln!("websocket send error: {}", e);
+            });
+        }
+    });
+
+    while let Some(message) = ws_rx.next().await {
         match message {
             Ok(message) if message.is_text() => {
-                println!("{new_id}: {message:?}");
-                tx.send(Message::text("JP2 byl wielkim Polakiem"))
-                    .await
-                    .unwrap();
+                let text = message.to_str().unwrap();
+                let users = users.clone();
+
+                match text {
+                    "green" => {
+                        users.write().await.get_mut(&new_id).unwrap().feeback =
+                            Some(UserFeedback::Green);
+                        broadcast_user_list(&users).await;
+
+                        tokio::spawn(async move {
+                            tokio::time::sleep(Duration::from_secs(10)).await;
+                            users
+                                .clone()
+                                .write()
+                                .await
+                                .get_mut(&new_id)
+                                .unwrap()
+                                .feeback = None;
+                            broadcast_user_list(&users).await;
+                        });
+                    }
+                    "red" => {
+                        users.write().await.get_mut(&new_id).unwrap().feeback =
+                            Some(UserFeedback::Red);
+                        broadcast_user_list(&users).await;
+
+                        tokio::spawn(async move {
+                            tokio::time::sleep(Duration::from_secs(10)).await;
+                            users
+                                .clone()
+                                .write()
+                                .await
+                                .get_mut(&new_id)
+                                .unwrap()
+                                .feeback = None;
+                            broadcast_user_list(&users).await;
+                        });
+                    }
+                    "2137" => {
+                        chan_tx
+                            .send(Message::text("JP2 byl wielkim Polakiem"))
+                            .unwrap();
+                    }
+                    _ => (),
+                }
             }
             Ok(message) if message.is_close() => {
-                tx.close().await.unwrap();
                 break;
             }
             _ => (),
         }
     }
+
+    users.write().await.remove(&new_id);
     println!("socket to {new_id} closed");
+
+    broadcast_user_list(&users).await;
+}
+
+async fn broadcast_user_list(users: &Users) {
+    let users_string = users
+        .read()
+        .await
+        .iter()
+        .map(|(k, v)| {
+            let value_string = v
+                .feeback
+                .as_ref()
+                .map(|feedback| format!(" - {}", feedback.to_string()))
+                .unwrap_or("".to_string());
+            let key = k.to_string();
+            format!("{key}{value_string}")
+        })
+        .fold(String::new(), |a, b| a + &b + "\n");
+
+    for user in users.read().await.values() {
+        user.tx
+            .send(Message::text(format!("list of users:\n{users_string}")))
+            .unwrap();
+    }
 }
--- static/index.js
+++ static/index.js
@@ -1,5 +1,28 @@
+const userList = document.querySelector("#users");
 const ws = new WebSocket("ws://localhost:3030/ws");
 
 setInterval(() => ws.send("2137"), 5000);
 
-ws.onmessage((message) => console.log(message));
+ws.onmessage = (message) => {
+    if (message.data.startsWith("your id:")) {
+        let id = message.data.split(": ")[1];
+        document.querySelector("#yourid").innerHTML = id;
+    }
+    if (message.data.startsWith("list of users:")) {
+        userList.innerHTML = "";
+        let users = message.data.trim().split("\n").slice(1);
+        users.forEach((user) => {
+            let li = document.createElement("p");
+            li.append(user);
+            userList.appendChild(li);
+        });
+    }
+};
+
+document.querySelector("#green").addEventListener("click", (e) => {
+    ws.send("green");
+});
+
+document.querySelector("#red").addEventListener("click", (e) => {
+    ws.send("red");
+});
